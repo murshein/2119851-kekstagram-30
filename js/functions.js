50dd9d864b869b1f9a3f29d46e61c36065255fe9
timurshoshkein <markblackswan@gmail.com>
2023-10-12 15:00:28 +0500

    fixed linter problems

--- js/functions.js
+++ js/functions.js
@@ -4,22 +4,30 @@
 function checkLength(text, maxLength) {
   return text.length <= maxLength;
 }
+checkLength('проверяемая строка', 20);
+checkLength('проверяемая строка', 18);
+checkLength('проверяемая строка', 10);
 
 //Функция для проверки, является ли строка палиндромом.
 //Палиндром — это слово или фраза, которые одинаково читаются и слева направо и справа налево.
 function checkIsPalindrome(string) {
-  let lowRegStr = string.replaceAll(' ','').toLowerCase();
-  let reverseStr = lowRegStr.split('').reverse().join('');
+  const lowRegStr = string.replaceAll(' ','').toLowerCase();
+  const reverseStr = lowRegStr.split('').reverse().join('');
   return lowRegStr === reverseStr;
 }
+
+checkIsPalindrome('топот');
+checkIsPalindrome('ДовОд');
+checkIsPalindrome('Кекс');
+checkIsPalindrome('Лёша на полке клопа нашёл ');
 
 //Функция принимает строку, извлекает содержащиеся в ней цифры от 0 до 9 и возвращает их в виде целого положительного числа.
 //Если в строке нет ни одной цифры, функция должна вернуть NaN.
 function parseNumber(string) {
-  let str = string.toString();
+  const str = string.toString();
   let result = '';
   for (let i = 0; i < str.length; i++) {
-    let num = parseInt(str[i], 10);
+    const num = parseInt(str[i], 10);
     if (!Number.isNaN(num)) {
       result += num;
     }
@@ -30,3 +38,11 @@ function parseNumber(string) {
     return NaN;
   }
 }
+
+parseNumber('2023 год');
+parseNumber('ECMAScript 2022');
+parseNumber('1 кефир, 0.5 батона');
+parseNumber('агент 007');
+parseNumber(2023);
+parseNumber(-1);
+parseNumber(1.5);
